cabbdc587cc4397e7f36089e569d562704fb32c5
shikhar <shikhar@Shikhars-MacBook-Pro.local>
2017-04-28 15:50:54 -0500

    added header component

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+//Header.js - header for our albums
+
+// import library
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// create components
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+      <View style={viewStyle}>
+        <Text style={textStyle}>{props.headerText}</Text>
+      </View>
+    );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+};
+
+// register component so it is available.
+export default Header;
